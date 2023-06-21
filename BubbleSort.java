279362a33321dde6d6e4e0c1a4363718ac6a7156
Abdul <abdulkaleem8360@gmail.com>
2023-06-21 21:21:46 +0530

    Added Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Bubble {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void bubble(int[] arr) {
+        boolean isswapped = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isswapped = true;
+                }
+
+            }
+            if (!isswapped)
+                break;
+
+        }
+    }
+}
