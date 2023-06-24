6ca8a407a63d0890fdd51ecfaf156a7318331b46
Abdul <abdulkaleem8360@gmail.com>
2023-06-24 18:59:20 +0530

    Added Cyclic Sort

---
+++ CyclicSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] array={5,4,3,4, 1};
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+    static void sort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            if(arr[i] != arr[arr[i]-1]){
+                swap(arr, i, arr[i]-1);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    static void swap(int[] ar,int s,int l){
+        int temp=ar[s];
+        ar[s]=ar[l];
+        ar[l]=temp;
+    }
+}
