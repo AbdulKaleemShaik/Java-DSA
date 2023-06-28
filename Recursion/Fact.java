8450223e285e8f34d8eb52b28f86026e81d88cbd
Abdul <abdulkaleem8360@gmail.com>
2023-06-29 00:00:43 +0530

    Added FactorialRecursion/Fact.java

---
+++ Recursion/Fact.java
@@ -0,0 +1,13 @@
+ class factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+    static int fact(int n){
+        if(n==1){
+            return n;
+        }
+        return n*fact(n-1);
+        
+    }
+    
+}
