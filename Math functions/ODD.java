fe079056c43af9713b77aff51904502dd7d4ec0a
Abdul <abdulkaleem8360@gmail.com>
2023-07-06 08:57:11 +0530

    added od using BITS

---
+++ Math functions/ODD.java
@@ -0,0 +1,10 @@
+public class ODD {
+    public static void main(String[] args) {
+        System.out.println(ODD(45));
+
+
+    }
+    static boolean ODD(int n){
+        return (n&1)==1;
+    }
+}
