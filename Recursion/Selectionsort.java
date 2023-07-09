d59bc212c24f1f4faa37924a3077ec98330011c2
Abdul <abdulkaleem8360@gmail.com>
2023-07-09 15:01:10 +0530

    Added selection sort using recursion

--- Recursion/Selectionsort.java
+++ Recursion/Selectionsort.java
@@ -1,22 +1,30 @@
 import java.util.Arrays;
 
-public class bubblesort {
+public class Selectionsort {
     public static void main(String[] args) {
         int[] a = { 4, 3, 2, 1, 0 };
-        bubble(a, a.length - 1, 0);
+        selection(a, a.length, 0, 0);
         System.out.println(Arrays.toString(a));
     }
-    static void bubble(int[] arr, int r, int c) {
+
+    static void selection(int[] arr, int r, int c, int max) {
         if (r == 0) {
             return;
         }
         if (c < r) {
-            swap(arr, c, c + 1);
-            bubble(arr, r, c + 1);
+            if (arr[max] < arr[c]) {
+                selection(arr, r, c + 1, c);
+            } else {
+                selection(arr, r, c + 1, max);
+            }
+
         } else {
-            bubble(arr, r - 1, 0);
+            swap(arr, r - 1, max);
+
+            selection(arr, r - 1, 0, 0);
         }
     }
+
     static void swap(int[] array, int i, int j) {
         int temp = array[j];
         array[j] = array[i];
