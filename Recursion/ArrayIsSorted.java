bc74b7d8b5f73e91d03398ba91dafdff42228158
Abdul <abdulkaleem8360@gmail.com>
2023-07-20 10:09:22 +0530

    Added ArrayIsSorted or not using recursion

---
+++ Recursion/ArrayIsSorted.java
@@ -0,0 +1,18 @@
+public class ArrayIsSorted {
+    public static void main(String[] args) {
+        int[] ar = { 1, 2, 3, 4, 9,6,7,8 };
+        System.out.println(Sorted(ar, ar.length - 1));
+    }
+
+    static boolean Sorted(int[] arr, int n) {
+        if (n == 0) {
+            return true;
+        }
+        if (arr[n] > arr[n - 1]) {
+            return Sorted(arr, n - 1);
+        }
+        return false;
+
+    }
+
+}
