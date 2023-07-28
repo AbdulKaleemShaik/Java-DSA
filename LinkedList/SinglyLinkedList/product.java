2a762fc7d98ee8b41a7a23ef882b4b7e9f4f10ce
Abdul <abdulkaleem8360@gmail.com>
2023-07-28 10:46:21 +0530

    Addded product of linkedlist elments

---
+++ LinkedList/SinglyLinkedList/product.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+class Node {
+    int val;
+    Node next;
+
+    Node() {
+
+    }
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class product {
+    static Node head;
+
+    public void insert(int index, Node nod) {
+        Scanner sc = new Scanner(System.in);
+        if (index == 5) {
+            return;
+        }
+        if (head == null) {
+            System.out.println(index + " value");
+            int n = sc.nextInt();
+            Node node = new Node(n);
+            node.next = null;
+            head = node;
+            // display();
+            insert(index + 1, node);
+        } else {
+            System.out.println(index + " value");
+            int n = sc.nextInt();
+            Node node = new Node(n);
+            node.next = null;
+            nod.next = node;
+            // display();
+            insert(index + 1, node);
+        }
+
+    }
+
+    public static void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.print("END");
+        System.out.println();
+    }
+
+    public static int sum(int sum) {
+        Node temp = head;
+        while (temp != null) {
+            sum = sum * temp.val;
+            temp = temp.next;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        product lr = new product();
+
+        lr.insert(0, head);
+      //  lr.display();
+        System.out.println(sum(1));
+    }
+
+}
