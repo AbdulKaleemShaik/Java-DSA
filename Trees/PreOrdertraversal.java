82185b7fa0431aa094cf94d5f475d075caf7ae5a
Abdul <abdulkaleem8360@gmail.com>
2023-08-26 21:34:40 +0530

    Added preorder traversaling

---
+++ Trees/PreOrdertraversal.java
@@ -0,0 +1,41 @@
+public class PreOrdertraversal {
+    static int idx = -1;
+
+    public static Node buildTree(int[] rs) {
+        idx++;
+        if (rs[idx] == -1) {
+            return null;
+        }
+        Node newnode = new Node(rs[idx]);
+        newnode.left = buildTree(rs);
+        newnode.right = buildTree(rs);
+        return newnode;
+
+    }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args) {
+        int[] rt = { 9, 2, 3, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node nr = buildTree(rt);
+       // System.out.println(nr.left.left.data);
+       preorder(nr);
+    }
+}
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int dta) {
+        this.data = dta;
+        this.left = null;
+        this.right = null;
+    }
+
+}
