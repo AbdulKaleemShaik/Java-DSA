269cc2eeb24cde5704536b4f27b938fba417a174
Abdul <abdulkaleem8360@gmail.com>
2024-02-27 11:22:49 +0530

    Added permutation for the Ay

--- Recursion/Permutations.java
+++ Recursion/Permutations.java
@@ -1,19 +1,34 @@
-public class Permutations {
-    public static void main(String[] args) {
-        per("","abc");
+import java.util.*;
+
+public class permutations {
+    public static List<List<Integer>> per(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] ele = new boolean[nums.length];
+        rec(ans, ds, ele, nums);
+        return ans;
     }
-    static void per(String p,String up){
-        if(up.isEmpty()){
-            System.out.println(p);
+
+    public static void rec(List<List<Integer>> ans, List<Integer> ds, boolean[] ele, int[] nums) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        char ch=up.charAt(0);
-        for (int i = 0; i <= p.length(); i++) {
-            String f=p.substring(0, i);
-            String s=p.substring(i,p.length());
-            per(f+ch+s, up.substring(1));
-            
+        for (int i = 0; i < nums.length; i++) {
+            if (!ele[i]) {
+                ele[i] = true;
+                ds.add(nums[i]);
+                rec(ans, ds, ele, nums);
+                ds.remove(ds.size() - 1);
+                ele[i] = false;
+            }
         }
     }
-}
 
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3 };
+        List<List<Integer>> ans = per(arr);
+        System.out.println(ans);
+
+    }
+}
